9174e3538f08c91683b0994024289904580b3c82
micotom <michaelm3283@googlemail.com>
2021-06-09 00:09:13 +0200

    switched to getenv

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['STOCKS_PRJ_DJANGO_SECRET']
+SECRET_KEY = os.getenv['STOCKS_PRJ_DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
