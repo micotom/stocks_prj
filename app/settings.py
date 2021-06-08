4c9268e15caa714858c668af63d1d47a69734524
micotom <michaelm3283@googlemail.com>
2021-06-09 00:50:21 +0200

    retrieve env variables via dotenv file

--- app/settings.py
+++ app/settings.py
@@ -11,17 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('STOCKS_PRJ_DJANGO_SECRET')
+SECRET_KEY = os.getenv('STOCKS_PRJ_DJANGO_SECRET', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
